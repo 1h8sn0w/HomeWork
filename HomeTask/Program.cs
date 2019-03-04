32c4bae7ed51c7589c2bfbe34fa6f9a35d851cb3
Volodymyr <citynight.immortal@gmail.com>
2019-03-04 19:03:00 +0200

    19:02

--- HomeTask/Program.cs
+++ HomeTask/Program.cs
@@ -12,7 +12,8 @@ namespace HomeTask
         {
             Gps point1 = new Gps(50.1324, 51.12412);
             Gps point2 = new Gps(43.1212, 54.12412);
-            CCar Audi = new CCar("Audi", point1, 250, 10000, 2005);
+            CVehicle Audi = new CCar("Audi", point1, 250, 10000, 2005);
+            
             CCar Bently = new CCar("Bently", point2, 450, 1000000, 2019);
             Console.WriteLine(Audi);
             Console.WriteLine(Bently);
@@ -24,7 +25,7 @@ namespace HomeTask
             masVehicles[2] = Boing;
             foreach (var item in masVehicles)
             {
-                if ()
+                if (item.Price > 99999)
                 {
                     Console.WriteLine("Прайс больше 100000");
                 }
