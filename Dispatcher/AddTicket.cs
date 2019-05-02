17d73dc2fd4a1b5d7fb49662cf215d11d93b8b6d
1h8sn0w <citynight.immortal@gmail.com>
2019-05-02 20:40:05 +0300

    DB Ticket controll

---
+++ Dispatcher/AddTicket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dispatcher
+{
+    public partial class AddTicket : Form
+    {
+        public AddTicket()
+        {
+            InitializeComponent();
+        }
+
+        private void AddTicket_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "dispatcherDataSet4.Route". При необходимости она может быть перемещена или удалена.
+            this.routeTableAdapter4.Fill(this.dispatcherDataSet4.Route);
+            textBox2.Text = DateTime.Now.ToString();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void routeDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Dispatcher/TicketCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Dispatcher
+{
+    class TicketCreate : DbContext
+    {
+        public TicketCreate() : base("Dispatcher.Properties.Settings.DispatcherConnectionString")
+        {
+
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
